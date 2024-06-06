4e55d768aebf4fcddca972bae41644919100b475
Tofazzal26 <afranislamabir6789@gmail.com>
2024-06-06 23:36:23 +0600

    my delivery list cancel validation finish

--- src/Dashboard/MyDeliveryList/MyDeliveryList.jsx
+++ src/Dashboard/MyDeliveryList/MyDeliveryList.jsx
@@ -1,10 +1,117 @@
+import { useContext } from "react";
+import { AuthContext } from "./../../AuthProvider/AuthProvider";
+import useAxiosSecure from "./../../Hooks/useAxiosSecure/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import MyDeliveryTable from "./MyDeliveryTable";
+import Swal from "sweetalert2";
+
 const MyDeliveryList = () => {
+  const { user } = useContext(AuthContext);
+
+  const axiosSecure = useAxiosSecure();
+
+  const { data: AssignDelivery = [] } = useQuery({
+    queryKey: ["Assign", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/assignDelivery/${user?.email}`);
+      return res.data;
+    },
+  });
+
+  const handleAlreadyCancel = () => {
+    Swal.fire("Already Canceled");
+  };
+
+  const handleCancel = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't  to Cancel this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Cancel it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const newStatus = "Cancelled";
+        const result = await axiosSecure.patch(
+          `/deliveryManCancel/${id}`,
+          { newStatus },
+          { withCredentials: true }
+        );
+        console.log(result.data);
+        if (result.data.modifiedCount > 0) {
+          Swal.fire({
+            title: "Canceled!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
+
+  console.log(AssignDelivery);
+
   return (
     <div className="bg-[#ffffff] mt-6">
       <div className="p-12">
         <h1 className="text-2xl font-semibold mb-6 uppercase text-center">
           My Delivery List
         </h1>
+        <div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Booked User Name
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Receiver Name
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Booked User Phone
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Requested Delivery Date
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Approximate Delivery Date
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Receiver Phone Number
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Receivers Address
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Location
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Action
+                  </th>
+                  <th className="font-semibold text-[15px] text-black">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {AssignDelivery.map((assignData, indx) => (
+                  <MyDeliveryTable
+                    key={assignData._id}
+                    indx={indx}
+                    assignData={assignData}
+                    handleCancel={handleCancel}
+                    handleAlreadyCancel={handleAlreadyCancel}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/Dashboard/MyDeliveryList/MyDeliveryTable.jsx
@@ -0,0 +1,85 @@
+const MyDeliveryTable = ({
+  assignData,
+  indx,
+  handleCancel,
+  handleAlreadyCancel,
+}) => {
+  const {
+    name,
+    receiverName,
+    receiverPhone,
+    phone,
+    requestDate,
+    ApproximateDate,
+    parcelDeliveryAddress,
+    status,
+    _id,
+  } = assignData || {};
+
+  return (
+    <>
+      <tr>
+        <td>
+          {" "}
+          <h1 className="font-semibold text-[14px]">{indx + 1}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{name}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{receiverName}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{phone}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{requestDate}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{ApproximateDate}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{receiverPhone}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">{parcelDeliveryAddress}</h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">
+            <button className="bg-[#60a5fa] px-4 py-2 text-white rounded-md">
+              Open
+            </button>
+          </h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">
+            <button className="bg-[#60a5fa] px-4 py-2 text-white rounded-md">
+              Deliver
+            </button>
+          </h1>
+        </td>
+        <td>
+          <h1 className="font-semibold text-[14px]">
+            {status === "On The Way" ? (
+              <button
+                onClick={() => handleCancel(_id)}
+                className="bg-red-500 px-4 py-2 text-white rounded-md"
+              >
+                Cancel
+              </button>
+            ) : (
+              <button
+                onClick={handleAlreadyCancel}
+                className="bg-red-500 px-4 py-2 text-white rounded-md"
+              >
+                Cancel
+              </button>
+            )}
+          </h1>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default MyDeliveryTable;
