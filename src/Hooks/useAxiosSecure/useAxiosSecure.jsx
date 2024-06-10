5c24ea234767fde369bc673907d836754dac0eb6
Tofazzal26 <afranislamabir6789@gmail.com>
2024-06-10 10:54:15 +0600

    interceptor add validation finish

--- src/Hooks/useAxiosSecure/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -1,9 +1,42 @@
 import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:4000",
 });
 const useAxiosSecure = () => {
+  const { logOut } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      console.log(error);
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
